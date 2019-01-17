57c51e9e395823a15fd9dca02d599186f6912134
vkaznodi <vkaznodi@student.unit.ua>
2019-01-17 19:51:57 +0200

    need change func render

--- src/render.c
+++ src/render.c
@@ -17,9 +17,9 @@ t_vector	canvas_to_viewport(int x, int y)
 {
 	t_vector v;
 
-	v[0] = x * (float)VIEWPORT_WIDTH / SCREEN_WIDTH /g_app->screen.ssvalue;
-	v[1] = y * (float)VIEWPORT_HEIGHT / SCREEN_HEIGHT /g_app->screen.ssvalue;
-	v[2] =g_app->camera.fov;
+	v[0] = x * (float)VIEWPORT_WIDTH / SCREEN_WIDTH / g_app->screen.ssvalue;
+	v[1] = y * (float)VIEWPORT_HEIGHT / SCREEN_HEIGHT / g_app->screen.ssvalue;
+	v[2] = g_app->camera.fov;
 	return (v);
 }
 
@@ -32,22 +32,22 @@ void		render()
 
 	g_stoprendering = 0;
 	x = -1;
-	while (++x < SCREEN_WIDTH *g_app->screen.ssvalue)
+	while (++x < SCREEN_WIDTH * g_app->screen.ssvalue)
 	{
 		y = -1;
-		while (++y < SCREEN_HEIGHT *g_app->screen.ssvalue)
+		while (++y < SCREEN_HEIGHT * g_app->screen.ssvalue)
 		{
 			d = rotate(g_app->camera.direction, canvas_to_viewport(
 				x - SCREEN_WIDTH *g_app->screen.ssvalue / 2,
 					SCREEN_HEIGHT *g_app->screen.ssvalue / 2 - y));
-			ray.position =g_app->camera.position;
+			ray.position = g_app->camera.position;
 			ray.direction = d;
-			g_app->screen.sspixels[x + y * SCREEN_WIDTH *g_app->screen.ssvalue] =
-					trace_ray(&ray, 1,g_app->iterations);
+			g_app->screen.sspixels[x + y * SCREEN_WIDTH * g_app->screen.ssvalue] =
+					trace_ray(&ray, 1, g_app->iterations);
 		}
 		if (g_stoprendering)
 			return;
-		gtk_progress_bar_set_fraction((GtkProgressBar *)g_app->progressbar, (double)x / (SCREEN_WIDTH *g_app->screen.ssvalue));
+		gtk_progress_bar_set_fraction((GtkProgressBar *)g_app->progressbar, (double)x / (SCREEN_WIDTH * g_app->screen.ssvalue));
 		while (g_main_context_iteration(NULL, FALSE));		
 	}
 	ssaa();
