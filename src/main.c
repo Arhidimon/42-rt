cacba57ac99fa6f6218e60069524e5d13769811d
Arhidimon <arhidimon@ukr.net>
2019-01-17 19:12:50 +0200

    some norms

--- src/main.c
+++ src/main.c
@@ -42,30 +42,34 @@ void free_scene()
 	g_app->scene.lights == NULL;
 }
 
-void menu_open (GtkMenuItem *menuitem, gpointer user_data)
+void menu_open(GtkMenuItem *menuitem, gpointer user_data)
 {
 	GtkWidget *dialog;
-	GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
+	GtkFileChooserAction action;
 	gint res;
+	char *filename;
 
+	action = GTK_FILE_CHOOSER_ACTION_OPEN;
 	dialog = gtk_file_chooser_dialog_new ("Open File", g_app->window,
                                       action, "_Cancel", GTK_RESPONSE_CANCEL,
                                       "_Open", GTK_RESPONSE_ACCEPT,
                                       NULL);
+	res = gtk_dialog_run (GTK_DIALOG (dialog));
+	if (res == GTK_RESPONSE_ACCEPT)
+	{
+		GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
+		filename = gtk_file_chooser_get_filename (chooser);
+		free_scene();
+		ft_parser(filename);
+		render();
+		g_free (filename);
+	}
+	gtk_widget_destroy (dialog);
+}
 
-res = gtk_dialog_run (GTK_DIALOG (dialog));
-	printf("%i\n", res);
-if (res == GTK_RESPONSE_ACCEPT)
-  {
-    char *filename;
-    GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
-    filename = gtk_file_chooser_get_filename (chooser);
-    if (ft_parser(filename) == NULL)
-    	render();
-    g_free (filename);
-  }
-
-gtk_widget_destroy (dialog);
+void menu_quit(GtkMenuItem *menuitem, gpointer user_data)
+{
+	gtk_window_close(g_app->window);
 }
 
 gboolean draw_rt(GtkWidget *widget, cairo_t *cr, gpointer data)
@@ -74,7 +78,7 @@ gboolean draw_rt(GtkWidget *widget, cairo_t *cr, gpointer data)
   GdkRGBA color;
   GtkStyleContext *context;
  
-  guchar *pixels;
+  	guchar *pixels;
 	int rowstride;
 	unsigned int x, y;
 
@@ -82,7 +86,7 @@ gboolean draw_rt(GtkWidget *widget, cairo_t *cr, gpointer data)
 
   width = gtk_widget_get_allocated_width (widget);
   height = gtk_widget_get_allocated_height (widget);
- GdkPixbuf* pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB,
+ 	GdkPixbuf* pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB,
                 FALSE, BPS, SCREEN_WIDTH, SCREEN_HEIGHT);
 	rowstride = gdk_pixbuf_get_rowstride (pixbuf);
 	pixels = gdk_pixbuf_get_pixels(pixbuf);
@@ -102,55 +106,17 @@ gboolean draw_rt(GtkWidget *widget, cairo_t *cr, gpointer data)
 		x++;
 	}
 	GdkPixbuf  *scpixbuf = gdk_pixbuf_scale_simple(pixbuf, width, height, GDK_INTERP_NEAREST);
-	//GdkPixbuf  *scpixbuf;
-	//gdk_pixbuf_scale (pixbuf, scpixbuf,0,0, width, height, 0, 0, 1 , 1, GDK_INTERP_NEAREST);
   gdk_cairo_set_source_pixbuf(cr, scpixbuf, 0, 0);
     cairo_paint(cr);
     g_object_unref(G_OBJECT(scpixbuf));
     g_object_unref(G_OBJECT(pixbuf));
   gtk_render_background (context, cr, 0, 0, width, height);
-
-	
-  
-
   gtk_style_context_get_color (context,
                                gtk_style_context_get_state (context),
                                &color);
   gdk_cairo_set_source_rgba (cr, &color);
-
   cairo_fill (cr);
-
  return FALSE;
-	// static GdkPixbuf* pixbuf = NULL;
-	// guchar *pixels;
-	// int rowstride;
-	// unsigned int x, y;
-
-	// if (!pixbuf)
-	// 	pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB,
- //                FALSE, BPS, WIDTH, HEIGHT);
-	// rowstride = gdk_pixbuf_get_rowstride (pixbuf);
-	// x = 0;
-	// while(x < WIDTH)
-	// {
-	// 	y = 0;
-	// 	while (y < HEIGHT)
-	// 	{
-	// 		pixels[y * rowstride + x * 3] = rand() % 256;
-	// 		pixels[y * rowstride + x * 3 + 1] = rand() % 256;
-	// 		pixels[y * rowstride + x * 3 + 2] = rand() % 256;
-	// 		y++;
-	// 	}
-	// 	x++;
-	// }
-	// //cairo_t *cr;
- //    //cr = gdk_cairo_create(gtk_widget_get_window(widget));
- //    //    cr = gdk_cairo_create (da->window);
- //    gdk_cairo_set_source_pixbuf(cr, pixbuf, 0, 0);
- //    cairo_paint(cr);
- //    //    cairo_fill (cr);
- //    //cairo_destroy (cr);
-	// return FALSE;
 }
  
 gboolean keypress (GtkWidget *widget, GdkEventKey *event, gpointer data)
@@ -184,77 +150,3 @@ void on_window_main_destroy()
 {
     gtk_main_quit();
 }
-// =======
-
-
-// 
-
-// int		event_handler(t_app *app)
-// {
-// 	SDL_Event event;
-
-// 	key_handler(KEY_NONE);
-// 	while (SDL_PollEvent(&event))
-// 	{
-// 		if (event.type == SDL_QUIT)
-// 			return (1);
-// 		else if (event.type == SDL_KEYDOWN)
-// 		{
-// 			if (event.key.keysym.sym == SDLK_ESCAPE)
-// 				return (1);
-// 			else
-// 				key_handler(event.key.keysym.scancode);
-// 		}
-// 	}
-// 	key_handler1(app);
-// 	return (0);
-// }
-
-// void	main_loop(t_app *app)
-// {
-// 	app->framecount = 0;
-// 	app->frametimelast = SDL_GetTicks();
-// 	while (1)
-// 	{
-// 		if (event_handler(app))
-// 			break ;
-// 		clearinfo();
-// 		printinfo(app);
-// 		SDL_RenderClear(app->renderer);
-// 		SDL_UpdateTexture(app->maintexture, NULL, app->screen.pixels,
-// 		(int)SCREEN_WIDTH * sizeof(Uint32));
-// 		SDL_RenderCopy(app->renderer, app->maintexture, NULL, NULL);
-// 		print_fps(app, count_fps(app));
-// 		SDL_RenderPresent(app->renderer);
-// 	}
-// }
-
-// int		main(int argc, char **argv)
-// {
-// 	t_app *app;
-
-// 	app = initialize_app();
-// 	initialize_sdl(app);
-// 	if (argc > 1 && argv[1][0] == '1' && argv[1][1] == '\0')
-// 		testscene_1(app);
-// 	else if (argc > 1 && argv[1][0] == '2' && argv[1][1] == '\0')
-// 		testscene_2(app);
-// 	else if (argc > 1 && argv[1][0] == '3' && argv[1][1] == '\0')
-// 		testscene_3(app);
-// 	else if (argc > 1 && argv[1][0] == '4' && argv[1][1] == '\0')
-// 		testscene_4(app);
-// 	else
-// 		testscene_5(app);
-// 	printcontrols();
-// 	app->curobj = app->scene.primitives;
-// 	app->curlobj = app->scene.lights;
-// 	printinfo(app);
-// 	render(app);
-// 	main_loop(app);
-// 	SDL_DestroyTexture(app->maintexture);
-// 	SDL_DestroyRenderer(app->renderer);
-// 	SDL_DestroyWindow(app->window);
-// 	SDL_Quit();
-// 	return (0);
-// }
-
