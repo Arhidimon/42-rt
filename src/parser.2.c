0cd3675798b19558eb6df6cca32202bd0b5271eb
Arhidimon <arhidimon@ukr.net>
2019-01-17 20:12:45 +0200

    42

---
+++ src/parser.2.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.2.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apavelko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/17 18:55:50 by apavelko          #+#    #+#             */
+/*   Updated: 2019/01/17 18:55:54 by apavelko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "jsmn.h"
+
+int		ft_iswhitespace(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n'
+			|| c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	return (0);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (char)c)
+		if (!*s++)
+			return (NULL);
+	return ((char *)s);
+}
+
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (0);
+}
+
+char	*ft_strcpy(char *dst, const char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char *str;
+
+	if (!(str = (char*)malloc(sizeof(*str) * (ft_strlen(s1) + 1))))
+		return (NULL);
+	ft_strcpy(str, s1);
+	return (str);
+}
